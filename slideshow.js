3f5adc5e7fa383d5d2bab8797621860b1c9ced47
Killian Meehan <88002145+Fennarius@users.noreply.github.com>
2023-06-27 11:33:04 -0400

    fading slideshow

--- slideshow.js
+++ slideshow.js
@@ -4,9 +4,22 @@ window.addEventListener("DOMContentLoaded", function () {
     var slideshowImage = document.getElementById("slideshow-image");
   
     function changeSlide() {
+      slideshowImage.style.opacity = 0;
       slideshowImage.src = "resources/" + images[currentIndex];
+      fadeIn(slideshowImage);
       currentIndex = (currentIndex + 1) % images.length;
     }
   
-    setInterval(changeSlide, 3000); 
+    function fadeIn(element) {
+      var opacity = 0;
+      var timer = setInterval(function () {
+        if (opacity >= 1) {
+          clearInterval(timer);
+        }
+        element.style.opacity = opacity;
+        opacity += 0.1;
+      }, 50);
+    }
+  
+    setInterval(changeSlide, 4500); 
   });
